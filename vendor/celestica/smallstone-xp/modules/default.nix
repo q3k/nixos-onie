2fed68673cc560e6b8994a9d3d17fc11e0dc2c2c
Sergiusz Bazanski <q3k@q3k.org>
2020-06-23 23:06:29 +0200

    smallstone-xp: fix intel gbe patch and apply
    This changes the behaviour of the mgmt iface LEDs, as per the patch
    comments (on/off for 10/100, off/on for 1000).

--- vendor/celestica/smallstone-xp/modules/default.nix
+++ vendor/celestica/smallstone-xp/modules/default.nix
@@ -2,4 +2,11 @@
   boot.kernelParams = [
     "console=ttyS0,115200"
   ];
+  boot.kernelPackages = pkgs.linuxPackages_5_6;
+  boot.kernelPatches = [
+    {
+      name = "celestica-driver-net-ethernet-intel-igb";
+      patch = ../celestica-driver-net-ethernet-intel-igb.patch;
+    }
+  ];
 }

0746e982f64a5900598c5ab63eee659202488857
Sergiusz Bazanski <q3k@q3k.org>
2020-06-24 00:12:25 +0200

    smallstone-xp: fix CPLD patch and apply
    This patch adds a CPLD control character device, ostensibly to talk to
    SFP modules, but with also some arbitrary register peeks/pokes.
    
    Untested. Not yet even sure what's normally consuming this.

--- vendor/celestica/smallstone-xp/modules/default.nix
+++ vendor/celestica/smallstone-xp/modules/default.nix
@@ -4,9 +4,14 @@
   ];
   boot.kernelPackages = pkgs.linuxPackages_5_6;
   boot.kernelPatches = [
-    {
-      name = "celestica-driver-net-ethernet-intel-igb";
+    { name = "celestica-driver-net-ethernet-intel-igb";
       patch = ../celestica-driver-net-ethernet-intel-igb.patch;
+    }
+    { name = "driver-celestica-redstone-xp";
+      patch = ../driver-celestica-redstone-xp.patch;
+      extraConfig = ''
+        CEL_REDSTONE_XP_CPLD m
+      '';
     }
   ];
 }
